ded9d5654ad3fcf76da003c78e90b22267e39e25
serna37 <serna37@serna37noMacBook-Pro.local>
2023-11-21 13:34:53 +0900

    Update

--- install.sh
+++ install.sh
@@ -5,14 +5,36 @@ echo '############################################################'
 # TODO 全部shellか、手順をreadmeにするかなやむな
 
 repos=(
+"vim"
 "git"
+"lazygit"
+"node"
 "starship"
+"zsh-git-prompt"
+"zsh-autosuggestions"
+"romkatv/powerlevel10k/powerlevel10k"
+"exa"
+"zoxide"
+"bat"
+"fzf"
+"fd"
+"sd"
+"procs"
+"ripgrep"
+"python3"
+"go"
+"java11"
 )
+
+install_cmd="brew"
 
 for v in ${repos[@]}; do
     echo $v
+    install_cmd="${install_cmd} ${v}"
     # brew install $v
 done
+
+echo $install_cmd
 
 
 
