e8119b773db2cb3953e0be5eab852c82a83e7773
serna37 <serna37@serna37noMacBook-Pro.local>
2024-05-14 21:34:03 +0900

    update wezterm lua conf

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,35 +1,28 @@
 local wezterm = require 'wezterm'
-
 local config = {}
-
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- カラースキームの設定
-config.color_scheme = 'AdventureTime'
-
--- 背景透過
-config.window_background_opacity = 0.85
-
--- 最初からフルスクリーンで起動
-local mux = wezterm.mux
-wezterm.on("gui-startup", function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
-    window:gui_window():toggle_fullscreen()
-end)
-
--- ショートカットキー設定
-local act = wezterm.action
-config.keys = {
-  -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
-  {
-    key = 'f',
-    mods = 'SHIFT|META',
-    action = wezterm.action.ToggleFullScreen,
-  },
+-- 色
+config.color_scheme = 'Obsidian'
+config.window_background_opacity = 0.9
+config.colors = {
+    foreground = 'silver',
+    background = 'rgb(40, 40, 50)',
+    cursor_fg = 'rgb(0, 0, 0, 0.8)',
+    cursor_bg = 'rgb(160, 160, 160, 0.6)',
+    selection_fg = 'rgb(50, 170, 255, 0.9)',
+    selection_bg = 'rgb(171, 171, 171, 0.4)',
 }
 
+-- フォント
+config.font = wezterm.font("Droid Sans Mono for Powerline", {weight="Medium", stretch="Normal", style="Normal"})
+config.font_size = 12
+
+-- ウィンドウ
+config.initial_cols = 200
+config.initial_rows = 55
 
 return config
 
