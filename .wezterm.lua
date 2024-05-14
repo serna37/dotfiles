e0621cb9cb439d41364176d85c86bfc15d1fce25
serna37 <serna37@serna37noMacBook-Pro.local>
2024-05-14 20:42:22 +0900

    wip wezterm lua

---
+++ .wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- カラースキームの設定
+config.color_scheme = 'AdventureTime'
+
+-- 背景透過
+config.window_background_opacity = 0.85
+
+-- 最初からフルスクリーンで起動
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+end)
+
+-- ショートカットキー設定
+local act = wezterm.action
+config.keys = {
+  -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
+  {
+    key = 'f',
+    mods = 'SHIFT|META',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
+
+return config
+
