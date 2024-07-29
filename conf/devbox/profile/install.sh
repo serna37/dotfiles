1296dda4a43f4f30ff7b33faf48803ef4e73c9c1
serna37 <serna37@serna37noMacBook-Pro.local>
2024-07-29 18:58:59 +0900

    modify devbox initiation install

--- conf/devbox/profile/install.sh
+++ conf/devbox/profile/install.sh
@@ -18,15 +18,15 @@ npm install -g atcoder-cli
 # C++ スニペット
 mkdir -p ~/git/dotfiles/snippets/
 cp ~/snippets/* ~/git/dotfiles/snippets/
-
-# TODO ここのコピーができてない or 消えてる？
-# cpp.snippetsにはリダイレクトされてるから、library/snippet.shは悪くなさそう
-cp ~/snippets/* ~/.vim/UltiSnips/
+cp ~/git/dotfiles/snippets/* ~/.vim/UltiSnips
 
 # C++ ライブラリ
 git clone -b develop https://github.com/serna37/library
 cd library
 zsh snippets.sh
+# library/snipet.shはシンボリックリンクを無視するが、cpしているので
+# もっかいコピーする (cpp.snippetsに書き込むために、事前にも必要)
+cp ~/git/dotfiles/snippets/* ~/.vim/UltiSnips
 
 # vim initiation
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
