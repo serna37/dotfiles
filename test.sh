98f8c34e3d1666883ad60a8edcb0c8c609089548
さーな <95469388+serna37@users.noreply.github.com>
2024-07-30 12:20:33 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 OS_NAME=$(uname -s)
 if [[ $OS_NAME == "Darwin" ]]; then
-    /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/serna37/dotfiles/master/test-mac.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/serna37/dotfiles/master/test-mac.sh)"
 elif [[ $OS_NAME == "Linux" ]]; then
-    /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/serna37/dotfiles/master/test-docker.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/serna37/dotfiles/master/test-docker.sh)"
 fi
 
+echo "done"
