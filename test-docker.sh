11242d36b56e6d375aa17743382dcc5c3698bd94
serna37 <serna37@serna37noMacBook-Pro.local>
2024-07-30 12:56:49 +0900

    del

--- test-docker.sh
+++
@@ -1,1 +0,0 @@
-echo "docker installer dotfiles!!"
--- test.sh
+++
@@ -1,7 +0,0 @@
-OS_NAME=$(uname -s)
-if [[ $OS_NAME == "Darwin" ]]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/serna37/dotfiles/master/test-mac.sh)"
-elif [[ $OS_NAME == "Linux" ]]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/serna37/dotfiles/master/test-docker.sh)"
-fi
-
