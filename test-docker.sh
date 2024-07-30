66e8d3c0472c8ced73c93a2691341032b754e0dc
serna37 <serna37@serna37noMacBook-Pro.local>
2024-07-30 12:13:34 +0900

    add test setup files

---
+++ test-docker.sh
@@ -0,0 +1,1 @@
+echo "docker installer dotfiles!!"
---
+++ test.sh
@@ -0,0 +1,7 @@
+OS_NAME=$(uname -s)
+if [[ $OS_NAME == "Darwin" ]]; then
+    /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/serna37/dotfiles/master/test-mac.sh)"
+else if [[ $OS_NAME == "Linux" ]]; then
+    /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/serna37/dotfiles/master/test-docker.sh)"
+fi
+
