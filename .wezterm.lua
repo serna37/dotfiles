99aa65cf5cba0a90b922a02880942daaa61bd076
さーな <37serna37serna37serna@gmail.com>
2025-07-19 12:48:27 +0900

    modify

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,7 +19,7 @@ config.colors = {
 
 -- フォント
 config.font = wezterm.font("Droid Sans Mono for Powerline", {weight="Medium", stretch="Normal", style="Normal"})
-config.font_size = 12
+config.font_size = 13
 
 -- ウィンドウ
 config.initial_cols = 150
