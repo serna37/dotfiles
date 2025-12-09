a026ee77dc8505ce7d2c97b46deb025fa282c3d3
さーな <37serna37serna37serna@gmail.com>
2025-12-09 18:42:15 +0900

    add template

---
+++ conf/cpp/template.cpp
@@ -0,0 +1,11 @@
+#ifdef LOCAL
+#include <bits/debug.hpp>
+#else
+#define debug(...)
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
